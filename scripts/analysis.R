42eabd00187be6f4239f2fcbf28e65bcb75451b7
gp1981 <45032495+gp1981@users.noreply.github.com>
2025-06-01 22:38:23 +0200

    FIxed CAGR with without Debt

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -643,40 +643,54 @@ full_equity_CAGR <- function(df){
   
   
   # Calculate full equity and CAGR
-  df <- df %>%
-    group_by(symbol) %>% 
+  df_cagr <- df %>%
+    group_by(Ticker) %>% 
     arrange(date) %>% # Ensure data is sorted by date
     mutate(
       # Get the last (newest) values
-      last_equity = last(totalStockholdersEquity),
-      last_totalDebt = coalesce(last(totalDebt),0),
+      last_equity = last(
+        coalesce(totalStockholdersEquity, lag(totalStockholdersEquity, 1))
+      ),
       
-      # Get the first (oldest) values
-      first_totalDebt = coalesce(first(totalDebt),0),
+      # Cumulative financial items
+      cum_dividends = cumsum(coalesce(commonDividendsPaid, 0) + coalesce(preferredDividendsPaid, 0)),
+      cum_issued = cumsum(coalesce(commonStockIssuance, 0)),
+      cum_repurchased = cumsum(coalesce(commonStockRepurchased, 0)),
       
-      # Calculate cumulative sums for required fields up to each row
-      cum_dividends = cumsum(dividendsPaid),
-      cum_issued = cumsum(commonStockIssued),
-      cum_repurchased = cumsum(commonStockRepurchased),
-      change_totalDebt = last_totalDebt - first_totalDebt,
-      
+      # Calculate change in TotalDebt
+      totalDebt_interp = na.approx(totalDebt, x = date, na.rm = FALSE),
+      change_totalDebt = totalDebt_interp - lag(totalDebt_interp,1),
+      cum_change_totalDebt = cumsum(coalesce(change_totalDebt, 0)),
       
       # Calculate full equity using the first value of totalStockholdersEquity
-      full_equity = last_equity + (-1) * cum_dividends - cum_issued + (-1) * cum_repurchased - change_totalDebt
-    ) %>%
-    # Calculate CAGR
+      full_equity = totalStockholdersEquity + (-1) * cum_dividends - cum_issued + (-1) * cum_repurchased - 
+        cum_change_totalDebt,
+      full_equity_noDebt = totalStockholdersEquity + (-1) * cum_dividends - cum_issued + (-1) * cum_repurchased 
+    ) %>% 
+    ungroup()
+  
+  # Calculate CAGR
+  df_cagr <- df_cagr %>%
+    group_by(Ticker) %>% 
+    arrange(date) %>% # Ensure data is sorted by date  
     mutate(
-      first_full_equity = first(full_equity), # Full equity at the first quarter
+      first_totalStockholdersEquity = first(totalStockholdersEquity), # Full equity at the first quarter
       years_elapsed = as.numeric(difftime(date, first(date), units = "days")) / 365.25,
       CAGR.full.Equity = ifelse(years_elapsed > 0,
-                                (full_equity / first_full_equity)^(1 / years_elapsed) - 1,
-                                NA_real_) # Avoid divide-by-zero for the first quarter
+                                (full_equity / first_totalStockholdersEquity)^(1 / years_elapsed) - 1,
+                                NA_real_), # Avoid divide-by-zero for the first quarter
+      CAGR.full.Equity_noDebt = ifelse(years_elapsed > 0,
+                                       (full_equity_noDebt / first_totalStockholdersEquity)^(1 / years_elapsed) - 1,
+                                       NA_real_) # Avoid divide-by-zero for the first quarter
+      
     ) %>% 
+    select(-totalDebt_interp) %>% 
     ungroup() %>% 
     arrange(desc(date))
   
   
-  return(df)
+  return(df_cagr)
+
 }
 # Negative FCF --------------------------------------------------
 negative_FCF <- function(df){
